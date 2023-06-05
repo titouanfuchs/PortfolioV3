1223c38e131b3a494a9028dfcdae3227acee6117
Titouan FUCHS <titouan.fuchs@gmail.com>
2023-06-05 12:38:45 +0200

    added build.sh script

---
+++ front/build.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+
+NODE_ENV=production
+rm -r out out.zip
+yarn build && mkdir out
+cp -r ./.next/standalone/* ./.next/standalone/.next ./public ./out
+cp -r ./.next/static ./out/.next/static
+mv ./out/server.js ./out/app.js
+zip -r out.zip ./out
