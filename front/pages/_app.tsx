a1f5571982a6d519c5433aeb0de3cbfc8fbcc23f
Titouan FUCHS <titouan.fuchs@gmail.com>
2023-06-06 13:34:10 +0200

    added animation on navigation button

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -28,20 +28,12 @@ function MyApp({ Component, pageProps }: AppProps) {
 
             <div className="flex max-h-screen">
                 <div className="hidden md:flex w-[18rem] space-y-5 flex-col border justify-center">
-                    <div className="aspect-square w-full border">
-                        t
-                    </div>
-
-                    <div className="aspect-square w-full border">
-                        t
-                    </div>
-
-                    <div className="aspect-square w-full border">
-                        t
-                    </div>
-
-                    <div className="aspect-square w-full border">
-                        t
+                    <div className="aspect-square w-full transition-colors delay-200 duration-200 ease-in-out text-primary p-2 hover:text-light hover:bg-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" classname="w-full h-full" fill="currentColor"
+                             className="bi bi-house" viewBox="0 0 16 16">
+                            <path
+                                d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
+                        </svg>
                     </div>
                 </div>
 
