e439f74c5181b76a0d64b4a39cfb801df5a27672
Titouan FUCHS <titouan.fuchs@gmail.com>
2023-06-08 07:44:34 +0200

    added languages

--- front/components/Skills/Language.tsx
+++ front/components/Skills/Language.tsx
@@ -9,8 +9,9 @@ type LanguageProps = {
 
 const Language = ({title, image} : LanguageProps) => {
 
+    // @ts-ignore
     return <>
-        <div className="w-40 h-44 border overflow-hidden flex flex-col rounded-lg">
+        <div className="w-40 h-44 overflow-hidden flex flex-col rounded-lg hover:bg-emerald-900 transition-colors duration-500 ease-in-out">
 
             <div className="flex-grow p-2 overflow-hidden">
                 <img src={image} alt={title} className="w-full h-full" ></img>
